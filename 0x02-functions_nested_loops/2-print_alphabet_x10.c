994cb8f5822bc9e58ac322d5eb235069daf257bb
PowellFiador <powellmawuli124@gmail.com>
2023-02-21 17:02:20 -0800

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a aprogram that prints ten times the alphabet
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i = 0;
+
+	while (i <= 9)
+	{
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	_putchar(letter);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
