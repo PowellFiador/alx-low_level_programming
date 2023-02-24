a7920d3f95dfd70387ede6dff12cd7b151791488
PowellFiador <powellmawuli124@gmail.com>
2023-02-24 15:25:35 -0800

    0-isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File - main.h
+ * Author: Powell
+ * Desc: heaader contains prototypes for all functions
+ */
+int _putchar(char c);
+int _isupper(int c);
+
+
+
+
+
+
+
+
+
+#endif
