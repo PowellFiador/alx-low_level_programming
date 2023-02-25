9b4c0af1dcdc3787f86482bb5086f3d5c6d3c25b
PowellFiador <powellmawuli124@gmail.com>
2023-02-24 17:05:29 -0800

    6-lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line -  a function that draws a straight line
+ *@n: the number of _ characters to be printed out
+ */
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+	for (line = 0; line <= n; line++)
+	{
+	_putchar('_');
+	}
+	}
+	_putchar('\n');
+}
+
+
+
